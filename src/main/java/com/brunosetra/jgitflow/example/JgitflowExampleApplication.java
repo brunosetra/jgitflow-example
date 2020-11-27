01b9cb67ae72a549db9b4201e5e026537d814611
bruno.setra <bruno.setra@gmail.com>
2020-11-27 10:05:22 -0300

    first commit

---
+++ src/main/java/com/brunosetra/jgitflow/example/JgitflowExampleApplication.java
@@ -0,0 +1,13 @@
+package com.brunosetra.jgitflow.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JgitflowExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JgitflowExampleApplication.class, args);
+	}
+
+}
